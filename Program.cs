30b9d0747167c1a07838fdb875f37e7e20ae468f
Eduardo-072 <eduardosilvaaa072@gmail.com>
2023-09-02 01:07:23 -0300

    inverti o o nome e o sobrenome

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome;
+Console.Write("digite seu nome aqui...");
+
+nome = Console.ReadLine()!;
+
+string sobrenome;
+Console.Write("digite seu sobrenome aqui...");
+
+sobrenome = Console.ReadLine()!;
+
+
+Console.WriteLine($"nome completo {nome} {sobrenome}");
+Console.WriteLine($"nome catálogo {sobrenome.ToUpper()} {nome}");
